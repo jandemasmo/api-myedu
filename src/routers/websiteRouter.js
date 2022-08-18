79f9d919b5411977cac1614033463e6a2b523212
jandemasmo <jandemasmo@yahoo.com>
2022-08-17 21:35:31 -0300

    alterações no controller de envio de email

--- src/routers/websiteRouter.js
+++ src/routers/websiteRouter.js
@@ -4,5 +4,7 @@ const websiteController = require("../controllers/websiteController");
 
 
 router.get("/", websiteController.home )
+router.post("/contact", websiteController.contact )
+router.get("/teste", websiteController.teste )
 
 module.exports = router
